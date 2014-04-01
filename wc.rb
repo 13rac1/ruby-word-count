ebb3740bee1bdab823e27fad403cbbc0c8bb39e0
Brad Erickson <eosrei@gmail.com>
2014-04-01 13:49:39 -0700

    Create WCRubyResults class to store multiple file results and implement option defaults

--- wc.rb
+++ wc.rb
@@ -5,19 +5,11 @@ require 'optparse'
 class WCRuby
   # Program initialization
   def initialize
-    # Array to store command line options.
+    # Hash to store command line options.
     @options = {}
-    # Integer byte count
-    @bytes = 0
-    # Integer character count
-    @chars = 0
-    # Integer line count
-    @lines = 0
-    # Integer max line length
-    @max_line_length = 0    
-    # Integer word count
-    @words = 0
- 
+    # Hash of results keyed by filename
+    @results = {}
+
     # Run the program
     parse_options
     parse_input
@@ -35,17 +27,18 @@ class WCRuby
       opt.separator "more than one FILE is specified.  With no FILE, or when FILE is -,"
       opt.separator "read standard input.  A word is a non-zero-length sequence of characters"
       opt.separator "delimited by white space."
-      opt.separator "The options below may be used to select which counts are printed."
+      opt.separator "The options below may be used to select which counts are printed, always in"
+      opt.separator "the following order: newline, word, character, byte, maximum line length."
 
-      opt.on("-c","--bytes", "print the byte counts") do
+      opt.on("-c", "--bytes", "print the byte counts") do
         @options[:bytes] = true
       end
 
-      opt.on("-m","--chars", "print the character counts") do
+      opt.on("-m", "--chars", "print the character counts") do
         @options[:chars] = true
       end
 
-      opt.on("-l","--lines", "print the newline counts") do
+      opt.on("-l", "--lines", "print the newline counts") do
         @options[:lines] = true
       end
 
@@ -55,51 +48,98 @@ class WCRuby
         @options[:files0-from] = f
       end
 
-      opt.on("-L","--max-line-length", "print the length of the longest line") do
+      opt.on("-L", "--max-line-length", "print the length of the longest line") do
         @options[:max-line-length] = true
       end
 
-      opt.on("-w","--words","print the word counts") do
+      opt.on("-w", "--words","print the word counts") do
         @options[:words] = true
       end
 
-      opt.on("-h","--help", "display this help and exit") do
+      opt.on("-h", "--help", "display this help and exit") do
         puts opt_parser
         exit
       end
-
     end
-    opt_parser.parse!
-    # FIXME: Implement @option defaults
+
+    # Parse options into @options or fail.
+    begin
+      opt_parser.parse!
+    rescue OptionParser::InvalidOption => e
+      puts "#{__FILE__}: #{e}"
+      puts "Try '#{__FILE__} --help' for more information."
+      exit 1
+    end
+
+    # If no command line options have been set, provide the defaults.
+    if @options.count() == 0
+      @options[:lines] = true
+      @options[:words] = true
+      @options[:bytes] = true
+    end
   end
 
   # Parse STDIN or file contents to create statistics
   def parse_input
     # Enable binary read mode.
     ARGF.binmode
+
+    current_file = ARGF.filename
+    @results[ARGF.filename] = WCRubyResults.new
+
     # Loop through all of the characters in the input files.
     ARGF.chars do |char|
+      # If the filename has changed, create a new storage structure.
+      if current_file != ARGF.filename
+        @results[ARGF.filename] = WCRubyResults.new
+      end
+
       if @options[:bytes]
-        @bytes += 1
+        @results[ARGF.filename].bytes += 1
       end
       if @options[:chars]
-        @chars += 1
+        @results[ARGF.filename].chars += 1
       end
       # FIXME: Handle all new line characters?
       if @options[:lines] && char == "\n"
-        @lines += 1
+        @results[ARGF.filename].lines += 1
       end
-      #@max_line_length = 0    
+      #@max_line_length = 0
       if @options[:words] && char == ' '
-        @words += 1
-      end      
-    end    
-  end
-  
-  def display_output
-    puts " #{@lines} #{@words} #{@bytes}"
+        @results[ARGF.filename].words += 1
+      end
+    end
   end
 
+  def display_output
+    @results.each { |file, result|
+      puts " #{result.lines} #{result.words} #{result.bytes} #{file}"
+    }
+    exit 0
+  end
+
+end
+
+# Data structure to store result data per file
+class WCRubyResults
+  attr_accessor :bytes
+  attr_accessor :chars
+  attr_accessor :lines
+  attr_accessor :max_line_length
+  attr_accessor :words
+
+  def initialize
+    # Integer byte count
+    @bytes = 0
+    # Integer character count
+    @chars = 0
+    # Integer line count
+    @lines = 0
+    # Integer max line length
+    @max_line_length = 0
+    # Integer word count
+    @words = 0
+  end
 end
 
 # Run the program
